df6674406f5ebfb7015d742ad9508c27492ea75d
vkochatkov <vlad.kochatkov@gmail.com>
2023-05-04 15:28:25 +0300

    extended functionality of searching  projectMembers

--- src/modules/actions/foundUsers.ts
+++ src/modules/actions/foundUsers.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createAction, Dispatch } from 'redux-act';
+import { RootState } from '../store';
 import { IFoundUser, IFoundUsers } from '../types/users';
 import { changeSnackbarState } from './snackbar';
 
@@ -9,31 +10,34 @@ export const foundUsersSuccess = createAction<IFoundUsers>(
   'FOUND_USERS_SUCCESS'
 );
 
-export const searchUsers = (search: string) => async (dispatch: Dispatch) => {
-  try {
-    const response = await axios({
-      method: 'GET',
-      url: `${process.env.REACT_APP_BACKEND_URL}/users?search=${search}`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      cancelToken: httpSource.token,
-    });
+export const searchUsers =
+  (search: string) => async (dispatch: Dispatch, getState: () => RootState) => {
+    const { userId } = getState().user;
 
-    const foundUsers = response.data.users.map((user: IFoundUser) => ({
-      name: user.name,
-      email: user.email,
-      id: user.id,
-    }));
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `${process.env.REACT_APP_BACKEND_URL}/users?id=${userId}&search=${search}`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        cancelToken: httpSource.token,
+      });
 
-    dispatch(foundUsersSuccess({ foundUsers }));
-  } catch (e: any) {
-    dispatch(
-      changeSnackbarState({
-        id: 'error',
-        open: true,
-        message: `${e.response.data.message}. Перезавантажте сторінку`,
-      })
-    );
-  }
-};
+      const foundUsers = response.data.users.map((user: IFoundUser) => ({
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      }));
+
+      dispatch(foundUsersSuccess({ foundUsers }));
+    } catch (e: any) {
+      dispatch(
+        changeSnackbarState({
+          id: 'error',
+          open: true,
+          message: `${e.response.data.message}. Перезавантажте сторінку`,
+        })
+      );
+    }
+  };
