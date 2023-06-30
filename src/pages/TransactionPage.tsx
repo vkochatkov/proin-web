e9935ed14801218baa6710f6f9d0e50953bb25b9
vkochatkov <vlad.kochatkov@gmail.com>
2023-06-30 19:33:12 +0300

    fixed problem with reload the transaction page with different pid and transaction id

--- src/pages/TransactionPage.tsx
+++ src/pages/TransactionPage.tsx
@@ -13,21 +13,22 @@ import { SnackbarUI } from '../components/UIElements/SnackbarUI';
 import { getTransactionLabel } from '../utils/utils';
 import { AddClassifierInputComponent } from '../components/FormComponent/AddClassifierInputComponent';
 import { fetchTransactionById } from '../modules/actions/transactions';
+import { getCurrentProject } from '../modules/selectors/mainProjects';
 
 import '../index.scss';
 import './TransactionPage.scss';
 
 interface IProps {}
 
-const TransactionPage: React.FC<IProps> = () => { 
+const TransactionPage: React.FC<IProps> = () => {
   const currentTransaction = useSelector(getCurrentTransaction);
   const types = ['income', 'expenses', 'transfer'];
   const [selectedTypeValue, setSelectedTypeValue] = useState<string>(
-    currentTransaction ? currentTransaction.type : ''
+    currentTransaction ? currentTransaction.type : '',
   );
-  const [selectedClassifierValue, setSelectedClassifierValue] = useState<string>(
-    currentTransaction ? currentTransaction.classifier : ''
-  );
+  const currentProject = useSelector(getCurrentProject);
+  const [selectedClassifierValue, setSelectedClassifierValue] =
+    useState<string>(currentTransaction ? currentTransaction.classifier : '');
   const { pid, transactionId } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -42,7 +43,7 @@ const TransactionPage: React.FC<IProps> = () => {
         isValid: true,
       },
     },
-    true
+    true,
   );
 
   useEffect(() => {
@@ -50,6 +51,24 @@ const TransactionPage: React.FC<IProps> = () => {
 
     dispatch(fetchTransactionById(transactionId) as any);
   }, []);
+
+  useEffect(() => {
+    if (
+      !currentTransaction ||
+      (currentTransaction && !currentTransaction.id) ||
+      !currentProject ||
+      (currentProject && !currentProject._id)
+    )
+      return;
+
+    if (pid && currentProject._id !== pid) {
+      navigate('/');
+    }
+
+    if (pid && transactionId && transactionId !== currentTransaction.id) {
+      navigate(`/project-edit/${pid}`);
+    }
+  }, [pid, currentTransaction, currentProject, transactionId]);
 
   const handleCloseTransactionPage = () => {
     if (pid) {
@@ -62,52 +81,52 @@ const TransactionPage: React.FC<IProps> = () => {
 
   return (
     <>
-    <SnackbarUI />
-    <div className="container">
-      <Header>
-        <Button
-          size="small"
-          transparent={true}
-          icon={true}
-          customClassName="header__btn-close"
-          onClick={handleCloseTransactionPage}
-        >
-          <img src="/back.svg" alt="back_logo" className="button__icon" />
-        </Button>
-      </Header>
-      <Card>
-        <TransactionSelect
-          label={"Тип транзакції"}
-          keyValue={'type'}
-          selectedValue={selectedTypeValue}
-          setSelectedValue={setSelectedTypeValue}
-          values={types}
-          getTranslation={getTransactionLabel}
-        />
-         <TransactionSelect
-          label={"Класифікатор"}
-          keyValue={'classifier'}
-          selectedValue={selectedClassifierValue}
-          setSelectedValue={setSelectedClassifierValue}
-          values={currentTransaction.classifiers}
-        />
-       <AddClassifierInputComponent inputHandler={inputHandler} />
-        <InteractiveInput
-          label='Сума'
-          id="sum"
-          type="number"
-          inputHandler={inputHandler}
-          entity={currentTransaction}
-        />
-        <div style={style}>
+      <SnackbarUI />
+      <div className='container'>
+        <Header>
+          <Button
+            size='small'
+            transparent={true}
+            icon={true}
+            customClassName='header__btn-close'
+            onClick={handleCloseTransactionPage}
+          >
+            <img src='/back.svg' alt='back_logo' className='button__icon' />
+          </Button>
+        </Header>
+        <Card>
+          <TransactionSelect
+            label={'Тип транзакції'}
+            keyValue={'type'}
+            selectedValue={selectedTypeValue}
+            setSelectedValue={setSelectedTypeValue}
+            values={types}
+            getTranslation={getTransactionLabel}
+          />
+          <TransactionSelect
+            label={'Класифікатор'}
+            keyValue={'classifier'}
+            selectedValue={selectedClassifierValue}
+            setSelectedValue={setSelectedClassifierValue}
+            values={currentTransaction.classifiers}
+          />
+          <AddClassifierInputComponent inputHandler={inputHandler} />
           <InteractiveInput
-            id="description"
+            label='Сума'
+            id='sum'
+            type='number'
             inputHandler={inputHandler}
             entity={currentTransaction}
           />
-        </div>
-      </Card>
-    </div>
+          <div style={style}>
+            <InteractiveInput
+              id='description'
+              inputHandler={inputHandler}
+              entity={currentTransaction}
+            />
+          </div>
+        </Card>
+      </div>
     </>
   );
 };
